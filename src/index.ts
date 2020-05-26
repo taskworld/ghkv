2c4945c949026e9af9b87c411d6fc388e7207571
dtinth on Cloud9 <thai.p@taskworld.com>
2020-05-26 07:09:46 +0000

    It is working!

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,14 @@ export type GhkvCreateOptions = {
   accessToken: string
   owner: string
   repo: string
-  ref?: string
+  branch?: string
 }
 
 export type GhkvDataReference<T> = {
-  get: () => Promise<T | null>
-  set: (data: T, options?: { message?: string }) => Promise<void>
+  get: () => Promise<T | undefined>
+  set: (data: T, options?: { message?: string }) => Promise<T>
   update: (
-    updater: (data: T | null) => T,
+    updater: (data: T | undefined) => T,
     options?: { message?: string }
   ) => Promise<T>
 }
@@ -20,17 +20,19 @@ export class GhkvDataStore {
   private cache = new Map<string, GhkvDataReference<any>>()
   private owner: string
   private repo: string
-  private ref?: string
+  private branch?: string
   private octokit: Octokit
 
   constructor(options: GhkvCreateOptions) {
-    this.octokit = new Octokit({ auth: options.accessToken })
+    this.octokit = new Octokit({
+      auth: options.accessToken || process.env.GITHUB_TOKEN,
+    })
     this.owner = options.owner
     this.repo = options.repo
-    this.ref = options.ref
+    this.branch = options.branch
   }
   doc<T>(key: string): GhkvDataReference<T> {
-    const { owner, repo, ref } = this
+    const { owner, repo, branch } = this
     let doc = this.cache.get(key)
     if (!doc) {
       const path = key + '.json'
@@ -39,12 +41,14 @@ export class GhkvDataStore {
       const ensureCache = async ({ renew = false } = {}) => {
         if (!cache || renew) {
           try {
-            const { data } = await this.octokit.repos.getContents({
-              owner,
-              repo,
-              path,
-              ref,
-            })
+            const { data } = await this.octokit.repos
+              .getContents({
+                owner,
+                repo,
+                path,
+                ref: branch,
+              })
+              .catch(decorateErrorWithMessage(`getContents`))
             if (Array.isArray(data)) {
               throw new Error(`Did not expect "${path}" to be a directory.`)
             }
@@ -59,38 +63,66 @@ export class GhkvDataStore {
         }
         return cache
       }
+      const updateDoc = async (
+        updater: (data: T | undefined) => T,
+        message: string
+      ) => {
+        let attempt = 0
+        const retryDelays = [0, 1000, 2000, 5000, 10000]
+        for (;;) {
+          try {
+            const cache = await ensureCache({ renew: attempt > 0 })
+            const newData = updater(parseDoc(cache))
+            const newContent = Buffer.from(
+              JSON.stringify(newData, null, 2)
+            ).toString('base64')
+            const { data: result } = await this.octokit.repos
+              .createOrUpdateFile({
+                sha: cache.existing?.sha,
+                owner,
+                repo,
+                branch,
+                path,
+                content: newContent,
+                message: message,
+              })
+              .catch(decorateErrorWithMessage('createOrUpdateFile'))
+            cache.existing = { sha: result.content.sha, content: newContent }
+            return newData
+          } catch (error) {
+            if (error.status === 409 && attempt < 5) {
+              const retryDelay = retryDelays[attempt++]
+              if (retryDelay != null) {
+                await new Promise((resolve) => setTimeout(resolve, retryDelay))
+                continue
+              }
+            }
+            throw error
+          }
+        }
+      }
       const newDoc: GhkvDataReference<T> = {
         get: async () => {
-          const cache = await ensureCache({ renew: true })
+          const cache = await ensureCache({ renew: true }).catch(
+            decorateErrorWithMessage(`get(${key})`)
+          )
           return parseDoc(cache)
         },
         set: async (
           v,
           { message = `set(${key}) @ ${new Date().toJSON()}` } = {}
         ) => {
-          const cache = await ensureCache()
-          const newContent = Buffer.from(JSON.stringify(v, null, 2)).toString(
-            'base64'
+          return updateDoc(() => v, message).catch(
+            decorateErrorWithMessage(`set(${key})`)
           )
-          const { data: result } = await this.octokit.repos.createOrUpdateFile({
-            sha: cache.existing?.sha,
-            owner,
-            repo,
-            ref,
-            path,
-            content: newContent,
-            message: message,
-          })
-          cache.existing = { sha: result.content.sha, content: newContent }
         },
         update: async (
-          updater: (data: T | null) => T,
-          { message = `set(${key}) @ ${new Date().toJSON()}` } = {}
+          updater: (data: T | undefined) => T,
+          { message = `update(${key}) @ ${new Date().toJSON()}` } = {}
         ) => {
-          const cache = await ensureCache()
-          const newData = updater(parseDoc(cache))
-          await newDoc.set(newData, { message })
-          return newData
+          return updateDoc(updater, message).catch(
+            decorateErrorWithMessage(`update(${key})`)
+          )
         },
       }
       doc = newDoc
@@ -104,5 +136,14 @@ function parseDoc(cache: {
 }): any {
   return cache.existing
     ? JSON.parse(Buffer.from(cache.existing.content!, 'base64').toString())
-    : null
+    : undefined
+}
+
+function decorateErrorWithMessage(text: string) {
+  return (error: any) => {
+    if (typeof error?.message === 'string') {
+      error.message = `${text} > ${error.message}`
+    }
+    throw error
+  }
 }
